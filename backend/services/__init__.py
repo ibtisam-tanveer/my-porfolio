2555cb6fd577c69ec92a3ae585a4a3916641d698
Ibtisam <ibtisam@Ibtisams-MacBook-Pro.local>
2026-04-06 09:56:37 +0200

    first commit

--- backend/services/__init__.py
+++ backend/services/__init__.py
@@ -1,8 +1,4 @@
 from .vector_store import VectorStore
 from .llm_service import LLMService
-from .embedding_service import EmbeddingService
 
-__all__ = ["VectorStore", "LLMService", "EmbeddingService"]
-
-
-
+__all__ = ["VectorStore", "LLMService"]
